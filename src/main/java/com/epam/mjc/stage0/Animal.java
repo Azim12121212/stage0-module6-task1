5e3ff78e4089c08696d29cf974b2cc63e6d70704
Azimbek <omar.azimbek@gmail.com>
2024-01-09 03:31:12 +0600

    0.6.1 Commit

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,34 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+    public String getDescription() {
+        String fur = "";
+        String paw = "";
 
+        if (this.hasFur) {
+            fur = "a";
+        } else {
+            fur = "no";
+        }
+
+        if (this.numberOfPaws==0 || this.numberOfPaws==1) {
+            paw = "paw";
+        }
+
+        if (this.numberOfPaws>1) {
+            paw = "paws";
+        }
+
+        return "This animal is mostly " +
+                this.color + ". It has " +
+                this.numberOfPaws + " " + paw + " and " + fur + " fur.";
+    }
 }
--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,23 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal {
 
+    public Bird(String color, int numberOfPaws, boolean hasFur) {
+        super(color, numberOfPaws, hasFur);
+    }
+
+    public Bird() {
+        super("blue", 2, false);
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription() + " Moreover, it has 2 wings and can fly.";
+    }
+
+    public static void main(String[] args) {
+        Bird bird = new Bird();
+
+        System.out.print(bird.getDescription());
+    }
 }
